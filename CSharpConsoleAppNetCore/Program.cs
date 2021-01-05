73d8470275ec53992e0fecb175eed6472ab15e7e
Etienne Baudoux <etbaudou@microsoft.com>
2021-01-05 12:10:16 -0800

    added projects

---
+++ CSharpConsoleAppNetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpConsoleAppNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
